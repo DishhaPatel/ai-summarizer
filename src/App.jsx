a1baab329da44b214384801f24fb0bbc27de9917
Disha <dishupatel5499@gmail.com>
2023-07-30 18:48:55 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import Hero from './Components/Hero'
-import Demo from './Components/Demo'
-import './App.css'
+import React from "react";
+import Hero from "./Components/Hero";
+import Demo from "./Components/Demo";
+import "./App.css";
 
 const App = () => {
   return (
-   <main>
-    <div className='main'>
-      <div className='gradient'>
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
       </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
 
-     <div className='app'>
-      <Hero/>
-      <Demo/>
-
-     </div>
-    </div>
-   </main>
-
-  )
-}
-
-export default App
+export default App;
--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -1,47 +1,59 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuary } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url :'hello',
-    summary:'',
+    url: "",
+    summary: "",
   });
 
-  const handleSubmit = async(e)=>{
-    alert('submited')
-  }
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuary();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
-       <div className="flex flex-col w-full gap-2">
-         <form className="flex justify-center items-center" onSubmit={handleSubmit}
-         >
-         <img
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
             src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-6'
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
 
-        <input
-            type="text"
-            placeholder='Paste the article link'
+          <input
+            type="url"
+            placeholder="Paste the article link"
             value={article.url}
-            onChange={(e) => {
-              console.log("trigreed !")
-              setArticle({...article, url: e.target.value })}}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
-            className='h-32'
+            className="url_input peer"
           />
 
           <button
-          type="submit"
-          className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
           >
-          ↵ 
+            <p>↵</p>
           </button>
-         </form>
-       </div>
+        </form>
+      </div>
     </section>
-  ) 
-}
+  );
+};
 
-export default Demo
+export default Demo;
